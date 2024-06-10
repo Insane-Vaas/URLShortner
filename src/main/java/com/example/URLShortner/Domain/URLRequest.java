e731c86496a1fb0f8b29457ccb9bfd0327d5ef10
Insane-Vaas <hrithikr200@gmail.com>
2024-06-10 12:07:28 +0530

    URLShortner

---
+++ src/main/java/com/example/URLShortner/Domain/URLRequest.java
@@ -0,0 +1,14 @@
+package com.example.URLShortner.Domain;
+
+public class URLRequest {
+    private String url;
+
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
